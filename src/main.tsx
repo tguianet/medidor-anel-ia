3bbaf854f153fa5edac6dbcaf4fd84d5862f3291
tguianet <tiagotguianet@gmail.com>
2026-09-23 14:03:51 -0300

    Route /v2 to standalone clean test screen

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import AppV2 from "./v2/AppV2";
 import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    {window.location.pathname.startsWith("/v2") ? <AppV2 /> : <App />}
   </StrictMode>,
 );
